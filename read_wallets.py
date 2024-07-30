03b368fe8e80e132a0cbdd7d8f5872f27c16c0c3
Александр Занин <bsalexandrzanin@gmail.com>
2024-07-30 15:56:26 +0300

    Fix max gal_limit check and increase Arbitrum chain gasLimit

--- read_wallets.py
+++ read_wallets.py
@@ -23,9 +23,15 @@ def create_private_key(mnemonic):
 
 
 # Функция для отправки транзакций
-def send_transaction(network_url, network_id, from_address, private_key, to_address, gas_limit=31500, reserve=10000000000):
+def send_transaction(
+        network_url,
+        network_id,
+        from_address,
+        private_key,
+        to_address,
+        gas_limit,
+        reserve=10000000000):
     web3 = Web3(Web3.HTTPProvider(network_url))
-    account = web3.eth.account.from_key(private_key)
     balance = web3.eth.get_balance(from_address)
     gas_price = web3.eth.gas_price
 
@@ -44,7 +50,7 @@ def send_transaction(network_url, network_id, from_address, private_key, to_addr
 
     # Проверка на достаточность средств для покрытия стоимости газа
     if balance <= tx_cost:
-        return f"Insufficient funds to cover gas cost: balance {balance}, tx cost {tx_cost}"
+        return f"Недостаточно газа для транзакции: balance {balance}, tx cost {tx_cost}"
 
     # Расчет максимально возможного значения транзакции с дополнительным запасом
     max_value = balance - tx_cost - reserve  # Уменьшаем на резерв для обеспечения достаточного запаса
@@ -53,12 +59,16 @@ def send_transaction(network_url, network_id, from_address, private_key, to_addr
     tx['value'] = max_value
 
     # Подписание и отправка транзакции
-    try:
-        signed_tx = web3.eth.account.sign_transaction(tx, private_key)
-        tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-        return tx_hash.hex()
-    except Exception as e:
-        return f"Ошибка при отправке транзакции: {e}"
+    attempt_chain = 0
+    while attempt_chain < 5:
+        try:
+            signed_tx = web3.eth.account.sign_transaction(tx, private_key)
+            tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+            return tx_hash.hex()
+        except Exception as e:
+            tx['value'] -= 1000000000000
+            attempt_chain += 1
+            print(f"Ошибка при отправке транзакции: {e}")
 
 
 with open('wallets_to.json', 'r') as w_to:
@@ -67,13 +77,15 @@ with open('wallets_to.json', 'r') as w_to:
 
 for acc_key, acc_value in accounts.items():
     print(f"Аккаунт {acc_key}")
-    if int(acc_key) not in [34]:
-        continue
 
     for network_name, network_data in networks.items():
             reserve = 10000000000  # По умолчанию увеличенный резерв
+            gas_limit = 30000
             if network_name in ["Base", "Optimism", "Binance Smart Chain"]:
                 reserve = 20000000000  # Увеличиваем резерв еще больше для проблемных сетей
+
+            if network_name in "Arbitrum":
+                gas_limit = 100000
 
             attempt = 1
             max_attempts = 5  # Максимальное количество попыток
@@ -86,21 +98,22 @@ for acc_key, acc_value in accounts.items():
                         Web3.to_checksum_address(acc_value['address']),
                         create_private_key(acc_value['private_key']),
                         Web3.to_checksum_address(dict_address_to[acc_key]),
-                        gas_limit=31500,  # Используем увеличенный лимит газа
+                        gas_limit=gas_limit,  # Используем увеличенный лимит газа
                         reserve=reserve
                     )
                     print(f"Транзакция в сети {network_name}: {result}")
                     break  # Если транзакция успешна, выходим из цикла
                 except Exception as e:
                     print(f"Ошибка при работе с сетью {network_name} (попытка {attempt}): {e}")
-                    reserve += 5000000000  # Увеличиваем резерв на 5,000,000,000 wei
+                    reserve += 50000000000  # Увеличиваем резерв на 5,000,000,000 wei
+                    gas_limit += 20000
                     attempt += 1
 
             if attempt > max_attempts:
                 print(f"Не удалось выполнить транзакцию в сети {network_name} после {max_attempts} попыток")
 
             print('Ожидание между сетями внутри акка')
-            time_wait.random_wait(10, 15)
+            time_wait.random_wait(1, 5)
     print('Ожидание между акками')
-    time_wait.random_wait(20, 60)
+    time_wait.random_wait(10, 30)
     print('-------------------------------------------')
